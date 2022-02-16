7b9a9d2e1832d213a27d49898159e889f1f6e737
Nikolina Spirkovska <nikolinas@users.noreply.github.com>
2022-02-16 09:03:43 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!👋</div>
+  return <div style={{ display: 'flex', justifyContent : 'center', alignItems : 'center', height : 200, marginTop : '6rem', fontSize : '2rem' }}>Hello world!👋</div>
 }
