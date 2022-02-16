1001a343e0dbf55e299052a3dc42ff242055fd77
Nikolina Spirkovska <nikolinas@users.noreply.github.com>
2022-02-16 15:42:41 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Home() {
-  return <div style={{ display: 'flex', justifyContent : 'center', alignItems : 'center', height : 200, marginTop : '6rem', fontSize : '2rem' }}>Hello world!👋 <br/> <span style={{ fontSize: '1rem' }}>(coming soon)</span></div>
+  return <div style={{ display: 'flex', flexFlow : 'column', justifyContent : 'center', alignItems : 'center', height : 200, marginTop : '6rem', fontSize : '2rem' }}>Hello world!👋 <span style={{ fontSize: '1rem' }}>(coming soon)</span></div>
 }
